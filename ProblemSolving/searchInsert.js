2d553a6ebce273f2330401dfeeb31f6b4761bda2
SaulMoreyra <armhacam21@gmail.com>
2024-03-26 20:59:01 -0600

    feat(ps): add serarch insert position problem

---
+++ ProblemSolving/searchInsert.js
@@ -0,0 +1,28 @@
+/*
+Given a sorted array if distinct integers and a target value
+return the index if the target is found. If not return the index where it 
+would be if it were inserted in order
+
+You must write an algorithm with O (log n) run time complexity
+*/
+
+const searchInsert = (nums, target) => {
+  let left = 0;
+  let right = nums.length - 1;
+
+  while (left <= right) {
+    let mid = left + Math.floor((right - left) / 2);
+    if (nums[mid] === target) return mid;
+
+    if (nums[mid] > target) {
+      right = mid - 1;
+    } else {
+      left = mid + 1;
+    }
+  }
+  return left;
+};
+
+console.log(searchInsert([1, 3, 5, 6], 5)); // 2
+console.log(searchInsert([1, 3, 5, 6], 2)); // 1
+console.log(searchInsert([1, 3, 5, 6], 7)); // 4
